671521587ee4bc10969ab1839ffd6d00f4d55426
Jared <jared.darvilljackson@gmail.com>
2016-07-25 20:51:18 +1200

    Task 5

--- Components/Components/Components.h
+++ Components/Components/Components.h
@@ -49,130 +49,196 @@ namespace Components {
 		System::ComponentModel::Container ^components;
 	private: System::Windows::Forms::Button^  btnToggle;
 			 int count;
+	private: System::Windows::Forms::Button^  btnMoveBox;
+	private: System::Windows::Forms::TextBox^  txtMoveBox;
+	private: System::Windows::Forms::PictureBox^  picBox;
+
+
+
 			 bool isOn;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->btnGrow = (gcnew System::Windows::Forms::Button());
-			this->txtChange = (gcnew System::Windows::Forms::TextBox());
-			this->btnChange = (gcnew System::Windows::Forms::Button());
-			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
-			this->btnAdd = (gcnew System::Windows::Forms::Button());
-			this->btnToggle = (gcnew System::Windows::Forms::Button());
-			this->SuspendLayout();
-			// 
-			// btnGrow
-			// 
-			this->btnGrow->Location = System::Drawing::Point(127, 12);
-			this->btnGrow->Name = L"btnGrow";
-			this->btnGrow->Size = System::Drawing::Size(75, 23);
-			this->btnGrow->TabIndex = 0;
-			this->btnGrow->Text = L"Grow!";
-			this->btnGrow->UseVisualStyleBackColor = true;
-			this->btnGrow->Click += gcnew System::EventHandler(this, &Components::btnGrow_Click);
-			// 
-			// txtChange
-			// 
-			this->txtChange->Location = System::Drawing::Point(12, 62);
-			this->txtChange->Name = L"txtChange";
-			this->txtChange->Size = System::Drawing::Size(136, 20);
-			this->txtChange->TabIndex = 1;
-			// 
-			// btnChange
-			// 
-			this->btnChange->Location = System::Drawing::Point(180, 62);
-			this->btnChange->Name = L"btnChange";
-			this->btnChange->Size = System::Drawing::Size(145, 23);
-			this->btnChange->TabIndex = 2;
-			this->btnChange->Text = L"Change!";
-			this->btnChange->UseVisualStyleBackColor = true;
-			this->btnChange->Click += gcnew System::EventHandler(this, &Components::btnChange_Click);
-			// 
-			// listBox1
-			// 
-			this->listBox1->FormattingEnabled = true;
-			this->listBox1->Location = System::Drawing::Point(180, 112);
-			this->listBox1->Name = L"listBox1";
-			this->listBox1->Size = System::Drawing::Size(120, 95);
-			this->listBox1->TabIndex = 3;
-			// 
-			// btnAdd
-			// 
-			this->btnAdd->Location = System::Drawing::Point(73, 112);
-			this->btnAdd->Name = L"btnAdd";
-			this->btnAdd->Size = System::Drawing::Size(75, 23);
-			this->btnAdd->TabIndex = 4;
-			this->btnAdd->Text = L"Add Line";
-			this->btnAdd->UseVisualStyleBackColor = true;
-			this->btnAdd->Click += gcnew System::EventHandler(this, &Components::btnAdd_Click);
-			// 
-			// btnToggle
-			// 
-			this->btnToggle->Location = System::Drawing::Point(73, 183);
-			this->btnToggle->Name = L"btnToggle";
-			this->btnToggle->Size = System::Drawing::Size(75, 23);
-			this->btnToggle->TabIndex = 5;
-			this->btnToggle->Text = L"On";
-			this->btnToggle->UseVisualStyleBackColor = true;
-			this->btnToggle->Click += gcnew System::EventHandler(this, &Components::btnToggle_Click);
-			// 
-			// Components
-			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(337, 633);
-			this->Controls->Add(this->btnToggle);
-			this->Controls->Add(this->btnAdd);
-			this->Controls->Add(this->listBox1);
-			this->Controls->Add(this->btnChange);
-			this->Controls->Add(this->txtChange);
-			this->Controls->Add(this->btnGrow);
-			this->Name = L"Components";
-			this->Text = L"Components";
-			this->Load += gcnew System::EventHandler(this, &Components::Components_Load);
-			this->ResumeLayout(false);
-			this->PerformLayout();
+			 /// <summary>
+			 /// Required method for Designer support - do not modify
+			 /// the contents of this method with the code editor.
+			 /// </summary>
+			 void InitializeComponent(void)
+			 {
+				 this->btnGrow = (gcnew System::Windows::Forms::Button());
+				 this->txtChange = (gcnew System::Windows::Forms::TextBox());
+				 this->btnChange = (gcnew System::Windows::Forms::Button());
+				 this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+				 this->btnAdd = (gcnew System::Windows::Forms::Button());
+				 this->btnToggle = (gcnew System::Windows::Forms::Button());
+				 this->btnMoveBox = (gcnew System::Windows::Forms::Button());
+				 this->txtMoveBox = (gcnew System::Windows::Forms::TextBox());
+				 this->picBox = (gcnew System::Windows::Forms::PictureBox());
+				 (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picBox))->BeginInit();
+				 this->SuspendLayout();
+				 // 
+				 // btnGrow
+				 // 
+				 this->btnGrow->Location = System::Drawing::Point(169, 15);
+				 this->btnGrow->Margin = System::Windows::Forms::Padding(4);
+				 this->btnGrow->Name = L"btnGrow";
+				 this->btnGrow->Size = System::Drawing::Size(100, 28);
+				 this->btnGrow->TabIndex = 0;
+				 this->btnGrow->Text = L"Grow!";
+				 this->btnGrow->UseVisualStyleBackColor = true;
+				 this->btnGrow->Click += gcnew System::EventHandler(this, &Components::btnGrow_Click);
+				 // 
+				 // txtChange
+				 // 
+				 this->txtChange->Location = System::Drawing::Point(16, 76);
+				 this->txtChange->Margin = System::Windows::Forms::Padding(4);
+				 this->txtChange->Name = L"txtChange";
+				 this->txtChange->Size = System::Drawing::Size(180, 22);
+				 this->txtChange->TabIndex = 1;
+				 // 
+				 // btnChange
+				 // 
+				 this->btnChange->Location = System::Drawing::Point(240, 76);
+				 this->btnChange->Margin = System::Windows::Forms::Padding(4);
+				 this->btnChange->Name = L"btnChange";
+				 this->btnChange->Size = System::Drawing::Size(193, 28);
+				 this->btnChange->TabIndex = 2;
+				 this->btnChange->Text = L"Change!";
+				 this->btnChange->UseVisualStyleBackColor = true;
+				 this->btnChange->Click += gcnew System::EventHandler(this, &Components::btnChange_Click);
+				 // 
+				 // listBox1
+				 // 
+				 this->listBox1->FormattingEnabled = true;
+				 this->listBox1->ItemHeight = 16;
+				 this->listBox1->Location = System::Drawing::Point(240, 138);
+				 this->listBox1->Margin = System::Windows::Forms::Padding(4);
+				 this->listBox1->Name = L"listBox1";
+				 this->listBox1->Size = System::Drawing::Size(159, 116);
+				 this->listBox1->TabIndex = 3;
+				 // 
+				 // btnAdd
+				 // 
+				 this->btnAdd->Location = System::Drawing::Point(97, 138);
+				 this->btnAdd->Margin = System::Windows::Forms::Padding(4);
+				 this->btnAdd->Name = L"btnAdd";
+				 this->btnAdd->Size = System::Drawing::Size(100, 28);
+				 this->btnAdd->TabIndex = 4;
+				 this->btnAdd->Text = L"Add Line";
+				 this->btnAdd->UseVisualStyleBackColor = true;
+				 this->btnAdd->Click += gcnew System::EventHandler(this, &Components::btnAdd_Click);
+				 // 
+				 // btnToggle
+				 // 
+				 this->btnToggle->Location = System::Drawing::Point(97, 225);
+				 this->btnToggle->Margin = System::Windows::Forms::Padding(4);
+				 this->btnToggle->Name = L"btnToggle";
+				 this->btnToggle->Size = System::Drawing::Size(100, 28);
+				 this->btnToggle->TabIndex = 5;
+				 this->btnToggle->Text = L"On";
+				 this->btnToggle->UseVisualStyleBackColor = true;
+				 this->btnToggle->Click += gcnew System::EventHandler(this, &Components::btnToggle_Click);
+				 // 
+				 // btnMoveBox
+				 // 
+				 this->btnMoveBox->Location = System::Drawing::Point(16, 325);
+				 this->btnMoveBox->Name = L"btnMoveBox";
+				 this->btnMoveBox->Size = System::Drawing::Size(132, 23);
+				 this->btnMoveBox->TabIndex = 6;
+				 this->btnMoveBox->Text = L"Move Box";
+				 this->btnMoveBox->UseVisualStyleBackColor = true;
+				 this->btnMoveBox->Click += gcnew System::EventHandler(this, &Components::btnMoveBox_Click);
+				 // 
+				 // txtMoveBox
+				 // 
+				 this->txtMoveBox->Location = System::Drawing::Point(16, 297);
+				 this->txtMoveBox->Name = L"txtMoveBox";
+				 this->txtMoveBox->Size = System::Drawing::Size(132, 22);
+				 this->txtMoveBox->TabIndex = 7;
+				 // 
+				 // picBox
+				 // 
+				 this->picBox->BackColor = System::Drawing::SystemColors::HotTrack;
+				 this->picBox->Location = System::Drawing::Point(185, 297);
+				 this->picBox->Name = L"picBox";
+				 this->picBox->Size = System::Drawing::Size(59, 51);
+				 this->picBox->TabIndex = 8;
+				 this->picBox->TabStop = false;
+				 // 
+				 // Components
+				 // 
+				 this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+				 this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+				 this->ClientSize = System::Drawing::Size(449, 779);
+				 this->Controls->Add(this->picBox);
+				 this->Controls->Add(this->txtMoveBox);
+				 this->Controls->Add(this->btnMoveBox);
+				 this->Controls->Add(this->btnToggle);
+				 this->Controls->Add(this->btnAdd);
+				 this->Controls->Add(this->listBox1);
+				 this->Controls->Add(this->btnChange);
+				 this->Controls->Add(this->txtChange);
+				 this->Controls->Add(this->btnGrow);
+				 this->Margin = System::Windows::Forms::Padding(4);
+				 this->Name = L"Components";
+				 this->Text = L"Components";
+				 this->Load += gcnew System::EventHandler(this, &Components::Components_Load);
+				 (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picBox))->EndInit();
+				 this->ResumeLayout(false);
+				 this->PerformLayout();
 
-		}
+			 }
 #pragma endregion
-	private: System::Void Components_Load(System::Object^  sender, System::EventArgs^  e) 
+	private: System::Void Components_Load(System::Object^  sender, System::EventArgs^  e)
 	{
-		count = 0;
-		isOn = false;
+				 count = 0;
+				 isOn = false;
 	}
-	private: System::Void btnGrow_Click(System::Object^  sender, System::EventArgs^  e) 
+			 //Task 1
+	private: System::Void btnGrow_Click(System::Object^  sender, System::EventArgs^  e)
 	{
-		btnGrow->Width += 10;
-		if ((btnGrow->Left < 0)||(btnGrow->Right > this->Width))
-		{
-			btnGrow->Text = "Too Big!";
-		}
+				 btnGrow->Width += 10;
+				 if ((btnGrow->Left < 0) || (btnGrow->Right > this->Width))
+				 {
+					 btnGrow->Text = "Too Big!";
+				 }
 	}
-	private: System::Void btnChange_Click(System::Object^  sender, System::EventArgs^  e) 
+			 //Task 2
+	private: System::Void btnChange_Click(System::Object^  sender, System::EventArgs^  e)
 	{
-		btnChange->Text = txtChange->Text;
+				 btnChange->Text = txtChange->Text;
 	}
-	private: System::Void btnAdd_Click(System::Object^  sender, System::EventArgs^  e) 
+			 //Task 3
+	private: System::Void btnAdd_Click(System::Object^  sender, System::EventArgs^  e)
 	{
-		count++;
-		listBox1->Items->Add("This is line " + count);
+				 count++;
+				 listBox1->Items->Add("This is line " + count);
 	}
-	private: System::Void btnToggle_Click(System::Object^  sender, System::EventArgs^  e) 
+			 //Task 4
+	private: System::Void btnToggle_Click(System::Object^  sender, System::EventArgs^  e)
 	{
-		if (isOn)
-		{
-			btnToggle->Text = "On";
-		}
-		else
-		{
-			btnToggle->Text = "Off";
-		}
-		isOn = !isOn;
+				 if (isOn)
+				 {
+					 btnToggle->Text = "On";
+				 }
+				 else
+				 {
+					 btnToggle->Text = "Off";
+				 }
+				 isOn = !isOn;
 	}
-};
+			 //Task 5
+	private: System::Void btnMoveBox_Click(System::Object^  sender, System::EventArgs^  e)
+	{
+				 int steps = 0;
+				 if (txtMoveBox->Text->Length > 0)
+				 {
+					 steps = System::Int32::Parse(txtMoveBox->Text);
+					 for (int i = 0; i < steps; i++)
+					 {
+						 picBox->Left += 10;
+						 System::Threading::Thread::Sleep(100);
+					 }
+				 }
+	}
+	};
 }
